d52b19617dcead1755e8adf066735c442c0ba5a3
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-10 20:52:17 +0900

    Title: 데이터 교환, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240710/데이터 교환/exchange-data.c
+++ 240710/데이터 교환/exchange-data.c
@@ -2,17 +2,14 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a = 5, b = 6, c = 7, tmp_1 = 0, tmp_2 = 0;
+    int a = 5, b = 6, c = 7, tmp = 0;
 
-    tmp_1 = a;
+    tmp = a;
     a = c;
-    tmp_2 = c;
     c = b;
-    b = tmp_1;
+    b = tmp;
 
     printf("%d\n%d\n%d", a, b, c);
-
-
 
     return 0;
 }
