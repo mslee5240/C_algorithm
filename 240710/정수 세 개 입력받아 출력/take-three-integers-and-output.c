28f161b9ac8bb4f3dbcd003af6b1d9911c46f368
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-10 21:58:35 +0900

    Title: 정수 세 개 입력받아 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240710/정수 세 개 입력받아 출력/take-three-integers-and-output.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 0, b = 0, c = 0;
+
+    scanf("%d %d %d%*c", &a, &b, &c);
+
+    printf("%d %d %d", a, b, c);
+    
+    return 0;
+}
