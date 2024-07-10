f4e06628c05858852ef073be1ce643880ab02a25
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-10 20:16:40 +0900

    Title: 달에서 무게 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240710/달에서 무게 구하기/weight-on-the-moon.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int weight = 13;
+    double ratio = 0.165;
+
+    printf("%d * %.6lf = %.6lf", weight, ratio, weight * ratio);
+    
+    return 0;
+}
