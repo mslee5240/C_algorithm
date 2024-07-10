ad8a5d663909123b0184c60dc0d226969f50e05b
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-10 20:03:10 +0900

    Title: 변수 선언하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240710/변수 선언하기/declaring-variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 3;
+    char b = 'C'; // // 단일 문자는 작은 따옴표를 사용
+    printf("%d\n%c", a, b); // 정수: %d, 문자: %c, 문자열: %s, 부동 소수점: %f
+
+    return 0;
+}
