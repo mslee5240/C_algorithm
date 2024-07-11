ceaa99ac241f3efd1f6840f6db0a8926638a6c66
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-11 13:45:30 +0900

    Title: 입력받아 출력 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240711/입력받아 출력 2/input-and-output-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int head = 0, tail = 0;
+
+    scanf("%d-%d%*c", &head, &tail);
+
+    printf("%d%d", head, tail);
+    
+    return 0;
+}
