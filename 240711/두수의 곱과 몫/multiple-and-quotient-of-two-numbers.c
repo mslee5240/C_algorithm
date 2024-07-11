0f17e02d0c8df809d4137f7581aa7e88ef8102ad
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-11 14:50:37 +0900

    Title: 두수의 곱과 몫, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240711/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 0, b = 0;
+
+    scanf("%d %d", &a, &b);
+
+    printf("%d * %d = %d\n", a, b, a * b);
+    printf("%d / %d = %d\n", a, b, a / b);
+
+    return 0;
+}
