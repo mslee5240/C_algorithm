4357ac1509c8a6ea85e970b311a518548a01403b
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-11 16:30:26 +0900

    Title: 시력 검사 2, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240711/시력 검사 2/eye-test-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    double a = 0.0;
+    scanf("%lf%*c", &a);
+
+    if (a >= 1.0) {
+        printf("High");
+    }
+    else if (a >= 0.5) {
+        printf("Middle");
+    }
+    else {
+        printf("Low");
+    }
+    
+    return 0;
+}
