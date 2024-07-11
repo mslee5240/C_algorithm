d9f1391885c9122927a314aa763585372214beba
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-11 15:31:38 +0900

    Title: 체질량지수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240711/체질량지수/bmi.c
+++ 240711/체질량지수/bmi.c
@@ -3,10 +3,9 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     int h = 0, w = 0;
-    int b = 0.0;
-
     scanf("%d %d%*c", &h, &w);
 
+    int b = 0;
     b = (10000 * w) / (h * h);
 
     printf("%d\n", b);
