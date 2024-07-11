9a304cc855ee88e987dce7a703c763d21524646b
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-11 17:09:08 +0900

    Title: 두 번의 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240711/두 번의 연산/two-operations.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 0;
+    scanf("%d%*c", &a);
+
+    if (a % 2 == 1) {
+        a += 3;
+    }
+    if (a % 3 == 0) {
+        a /= 3;
+    }
+
+    printf("%d\n", a);
+    
+    return 0;
+}
