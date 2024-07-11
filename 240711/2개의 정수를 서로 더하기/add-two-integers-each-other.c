7c1a795d9a960c7c3a6ee7e9e7e63a9fb9535705
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-11 14:55:23 +0900

    Title: 2개의 정수를 서로 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240711/2개의 정수를 서로 더하기/add-two-integers-each-other.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 0, b = 0;
+
+    scanf("%d %d%*c", &a, &b);
+
+    a += b;
+    b += a;
+
+    printf("%d %d\n", a, b);
+    
+    return 0;
+}
