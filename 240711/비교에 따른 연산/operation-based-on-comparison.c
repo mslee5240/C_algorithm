6918c8b0373efbe06582fb7e7f53487fca2fd8ea
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-11 16:02:22 +0900

    Title: 비교에 따른 연산, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240711/비교에 따른 연산/operation-based-on-comparison.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 0, b = 0;
+    scanf("%d %d%*c", &a, &b);
+
+    if (a > b) {
+        printf("%d", a * b);
+    }
+    else {
+        printf("%d", b / a);
+    }
+    
+    return 0;
+}
