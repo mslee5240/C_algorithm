12b45cba6358c3ddce021a3ebb9f476f363e29c9
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-11 17:03:24 +0900

    Title: 최대 2번의 연산, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240711/최대 2번의 연산/up-to-2-calculations.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 0;
+    scanf("%d%*c", &a);
+
+    if (a % 2 == 0) {
+        a /= 2;
+    }
+    if (a % 2 == 1) {
+        a = (a + 1) / 2;
+    }
+
+    printf("%d\n", a);
+
+    return 0;
+}
