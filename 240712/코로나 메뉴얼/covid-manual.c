530c8b6c9573f111a85bb5552f4a86a83462ea8a
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-12 15:27:19 +0900

    Title: 코로나 메뉴얼, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240712/코로나 메뉴얼/covid-manual.c
+++ 240712/코로나 메뉴얼/covid-manual.c
@@ -4,26 +4,27 @@ int main() {
     // 여기에 코드를 작성해주세요.
     char sym_a = 0, sym_b = 0, sym_c = 0;
     int a = 0, b = 0, c = 0;
+    int cnt = 0;
 
     scanf("%c %d%*c", &sym_a, &a);
     scanf("%c %d%*c", &sym_b, &b);
     scanf("%c %d%*c", &sym_c, &c);
 
-    if (sym_a = 'Y' && a >= 37) {
-        if ((sym_b = 'Y' && b >= 37) || (sym_c == 'Y' && c >= 37)) {
-            printf("E");
-        }
-        else {
-            printf("N");
-        }
+    if (sym_a  == 'Y' && a > 37) {
+        cnt += 1;
     }
-    else if ((sym_b = 'Y' && b >= 37) && (sym_c == 'Y' && c >= 37)) {
+    if (sym_b  == 'Y' && b > 37) {
+        cnt += 1;
+    }
+    if (sym_c  == 'Y' && c > 37) {
+        cnt += 1;
+    }
+    if (cnt >= 2) {
         printf("E");
     }
     else {
         printf("N");
     }
-
 
     return 0;
 }
