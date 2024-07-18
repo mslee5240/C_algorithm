9ee4d79e19d92eb5f58df8832ad6969d1efb1b70
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-18 21:32:19 +0900

    Title: 369 게임, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240718/369 게임/369-game.c
+++ 240718/369 게임/369-game.c
@@ -5,8 +5,15 @@ int main() {
     int n = 0;
     scanf("%d%*c", &n);
 
-    for (int i = 1; i <= n; i++) {
-        if (i % 3 == 0 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+    int i = 1;
+    while (i < = n) {
+        if (i % 3 == 0) {
+            printf("0 ");
+        }
+        else if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) {
+            printf("0 ");
+        }
+        else if (i / 10 == 3 || i / 10 == 6 || i % 10 == 9) {
             printf("0 ");
         }
         else {
