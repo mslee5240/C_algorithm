621ae0ca97302873a57de97039bfcdac6868f679
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-19 23:56:26 +0900

    Title: 배수의 개수 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240719/배수의 개수 2/number-of-multipliers-2.c
+++ 240719/배수의 개수 2/number-of-multipliers-2.c
@@ -3,9 +3,10 @@
 int main() {
     // 여기에 코드를 작성해주세요.
     int cnt = 0, nInput = 0;
-    
+
     for (int i = 0; i < 10; i++) {
         scanf("%d", &nInput);
+        
         if (nInput % 2 == 1) {
             cnt++;
         }
