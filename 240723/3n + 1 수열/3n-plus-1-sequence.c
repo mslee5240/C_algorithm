5978f5480d4c0025ae81580225689271862a1803
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-23 20:20:47 +0900

    Title: 3n + 1 수열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240723/3n + 1 수열/3n-plus-1-sequence.c
+++ 240723/3n + 1 수열/3n-plus-1-sequence.c
@@ -6,23 +6,39 @@ int main() {
     scanf("%d", &n);
 
     while (1) {
-        
-        if (n % 2 == 0) {
-            n /= 2;
-            cnt++;
-            continue;
-        }
-        if (n % 2 == 1 && n != 1) {
-            n *= 3;
-            n++;
-            cnt++;
-            continue;
-        }
         if (n == 1) {
             printf("%d", cnt);
             break;
         }
 
+        if (n % 2 == 0) {
+            n /= 2;
+        }
+        else {
+            n = n * 3 + 1;
+        }
+
+        cnt++;
     }
+
+    // while (1) {
+        
+    //     if (n % 2 == 0) {
+    //         n /= 2;
+    //         cnt++;
+    //         continue;
+    //     }
+    //     if (n % 2 == 1 && n != 1) {
+    //         n *= 3;
+    //         n++;
+    //         cnt++;
+    //         continue;
+    //     }
+    //     if (n == 1) {
+    //         printf("%d", cnt);
+    //         break;
+    //     }
+
+    // }
     return 0;
 }
