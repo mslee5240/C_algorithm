9a36aeb7b76af3e307263530473d1410ebb5b0d9
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-23 17:51:23 +0900

    Title: 1부터의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/1부터의 합/sum-from-the-one.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n = 0, sum_val = 0;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= 100; i++) {
+        sum_val += i;
+        
+        if (sum_val >= n) {
+            printf("%d", i);
+            break;
+        }
+    }
+
+
+    return 0;
+}
