89e592ec53129cbbc932f5f97efa29ca858bbdb6
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-23 16:40:56 +0900

    Title: 입력받은 수의 합과 평균, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/입력받은 수의 합과 평균/sum-and-average-of-the-inputs.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n = 0;
+    scanf("%d", &n);
+
+    int nInput = 0, sum_val = 0, cnt = 0;
+
+    for (int i = 1; i <= n; i++) {
+        scanf("%d", &nInput);
+        sum_val += nInput;
+        cnt++;
+    }
+
+    printf("%d %.1f", sum_val, (double)sum_val / cnt);
+
+    return 0;
+}
