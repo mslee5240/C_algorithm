92b85e599c0d636033124237af27911b6ce1e4c1
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-23 19:52:58 +0900

    Title: 출석 부르기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/출석 부르기 2/calling-attendance-2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n = 0;
+
+    while (1) {
+        scanf("%d", &n);
+        
+        if (n == 1) {
+            printf("John\n");
+        }
+        else if (n == 2) {
+            printf("Tom\n");
+        }
+        else if (n == 3) {
+            printf("Paul\n");
+        }
+        else if (n == 4) {
+            printf("Sam\n");
+        }
+        else {
+            printf("Vacancy\n");
+            break;
+        }
+    }
+
+    return 0;
+}
