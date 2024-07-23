6c6c7f97f53795671857dcbb586926ab6d8e8c64
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-23 17:04:47 +0900

    Title: a의 b승, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/a의 b승/a-to-the-power-of-b.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a = 0, b = 0;
+    scanf("%d %d", &a, &b);
+
+    int prod = 1;
+
+    if (b == 0) {
+        a = 1;
+    }
+    else {
+        for (int i = 0; i < b; i++) {
+            prod *= a;
+        }
+    }
+
+    printf("%d", prod);
+    
+    return 0;
+}
