4a4814a32a2c607d4d2ddeab361b6aea476d7af3
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-23 18:13:40 +0900

    Title: 1까지 나누기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/1까지 나누기/divide-by-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n = 0;
+    scanf("%d", &n);
+
+    for (int i = 1; i <= 5000; i++) {
+        n /= i;
+        if (n <= 1) {
+            printf("%d", i);
+            break;
+        }
+    }
+    
+    return 0;
+}
