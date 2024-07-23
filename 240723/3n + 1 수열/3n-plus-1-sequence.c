249e84dc982e759494eee64e6b99242819671b5e
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-23 20:18:12 +0900

    Title: 3n + 1 수열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/3n + 1 수열/3n-plus-1-sequence.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n = 0, cnt = 0;
+    scanf("%d", &n);
+
+    while (1) {
+        
+        if (n % 2 == 0) {
+            n /= 2;
+            cnt++;
+            continue;
+        }
+        if (n % 2 == 1 && n != 1) {
+            n *= 3;
+            n++;
+            cnt++;
+            continue;
+        }
+        if (n == 1) {
+            printf("%d", cnt);
+            break;
+        }
+
+    }
+    return 0;
+}
