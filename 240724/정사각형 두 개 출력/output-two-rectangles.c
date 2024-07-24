21ef3d75edc25adf895973b1cba6f08dafe48095
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-24 14:52:01 +0900

    Title: 정사각형 두 개 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/정사각형 두 개 출력/output-two-rectangles.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n = 0;
+    scanf("%d", &n);
+
+    for (int k = 0; k < 2; k++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                printf("*");
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+        
+
+    return 0;
+}
