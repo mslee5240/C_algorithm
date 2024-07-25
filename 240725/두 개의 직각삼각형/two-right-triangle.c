9f3331a64b19c69b1f0e3df687ca4d9582681400
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-25 17:21:59 +0900

    Title: 두 개의 직각삼각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240725/두 개의 직각삼각형/two-right-triangle.c
+++ 240725/두 개의 직각삼각형/two-right-triangle.c
@@ -6,10 +6,10 @@ int main() {
     scanf("%d", &n);
 
     for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n -i; ++j) {
+        for (int j = 0; j < n - i; ++j) {
             printf("*");
         }
-        for (int j = 0; j < i*2; ++j) {
+        for (int j = 0; j < i * 2; ++j) {
             printf(" ");
         }
         for (int j = 0; j < n - i; ++j) {
