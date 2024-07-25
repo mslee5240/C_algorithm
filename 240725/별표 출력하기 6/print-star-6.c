309c6f765f42c2a3566a2be1282f49c33c453e48
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-25 17:48:00 +0900

    Title: 별표 출력하기 6, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240725/별표 출력하기 6/print-star-6.c
+++ 240725/별표 출력하기 6/print-star-6.c
@@ -19,7 +19,7 @@ int main() {
         for (int j = 0; j < n-i-2; ++j) {
             printf("  ");
         }
-        for (int j = 0; j < n+2*i-1; j++) {
+        for (int j = 0; j < (n - 1) + (2 * i); j++) {
             printf("* ");
         }
         printf("\n");
