d3217d832aec5fce4b712ebd73a3f8fe19aa0c7e
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-25 17:52:04 +0900

    Title: 별표 출력하기 6, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240725/별표 출력하기 6/print-star-6.c
+++ 240725/별표 출력하기 6/print-star-6.c
@@ -5,24 +5,50 @@ int main() {
     int n = 0;
     scanf("%d", &n);
 
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < i; ++j) {
-            printf("  ");
-        }
-        for (int j = 0; j < (n*2) - (i*2) - 1; j++) {
-            printf("* ");
-        }
-        printf("\n");
-    }
+    for (int i = 0; i < 2 * n - 1; ++i) {
+        int stars, spaces;
 
-    for (int i = 0; i < n - 1; ++i) {
-        for (int j = 0; j < n-i-2; ++j) {
+        if (i < n) {
+            // 위쪽 절반
+            spaces = i;                   // 공백의 개수
+            stars = (n * 2) - (i * 2) - 1; // 별의 개수
+        } else {
+            // 아래쪽 절반
+            spaces = 2 * n - i - 2;       // 공백의 개수
+            stars = (i - n + 1) * 2 + 1;  // 별의 개수
+        }
+
+        // 공백 출력
+        for (int j = 0; j < spaces; ++j) {
             printf("  ");
         }
-        for (int j = 0; j < 3 + (2 * i); j++) {
+
+        // 별 출력
+        for (int j = 0; j < stars; ++j) {
             printf("* ");
         }
+        
+        // 줄 바꿈
         printf("\n");
     }
+    // for (int i = 0; i < n; ++i) {
+    //     for (int j = 0; j < i; ++j) {
+    //         printf("  ");
+    //     }
+    //     for (int j = 0; j < (n*2) - (i*2) - 1; j++) {
+    //         printf("* ");
+    //     }
+    //     printf("\n");
+    // }
+
+    // for (int i = 0; i < n - 1; ++i) {
+    //     for (int j = 0; j < n-i-2; ++j) {
+    //         printf("  ");
+    //     }
+    //     for (int j = 0; j < 3 + (2 * i); j++) {
+    //         printf("* ");
+    //     }
+    //     printf("\n");
+    // }
     return 0;
 }
