9875f171f68444731d1143e660ff0aed1e3c6716
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-25 16:10:51 +0900

    Title: 별표 출력하기 3, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240725/별표 출력하기 3/print-star-3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n = 0;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < i; ++j) {
+            printf("  ");
+        }
+        for (int j = 0; j < (n * 2 - 1) - i * 2; ++j) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
