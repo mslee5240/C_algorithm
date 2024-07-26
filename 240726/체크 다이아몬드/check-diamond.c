402353c1f3daf3b22c0f451d8e8be095b866430f
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-26 18:03:29 +0900

    Title: 체크 다이아몬드, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/체크 다이아몬드/check-diamond.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n = 0;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n - 1 - i; ++j) {
+            printf(" ");
+        }
+        for (int j = 0; j < i + 1; ++j) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    for (int i = n - 2; i >= 0; --i) {
+        for (int j = 0; j < n - 1 - i; ++j) {
+            printf(" ");
+        }
+        for (int j = 0; j < i + 1; ++j) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
