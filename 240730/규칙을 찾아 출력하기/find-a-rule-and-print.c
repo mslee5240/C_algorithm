34306b91468ad8299c55e5f832f6c1a4c4a59aa9
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-30 16:43:15 +0900

    Title: 규칙을 찾아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240730/규칙을 찾아 출력하기/find-a-rule-and-print.c
+++ 240730/규칙을 찾아 출력하기/find-a-rule-and-print.c
@@ -7,7 +7,7 @@ int main() {
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            if ((i == 0 || j == 4) || i > j) {
+            if ((i == 0 || j == n -1 ) || i > j) {
                 printf("* ");
             }
             else {
