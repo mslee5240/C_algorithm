7a83790f4d71abe73678dbb077ce348202251fcd
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-07-30 16:19:33 +0900

    Title: 별표 출력하기 12, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240730/별표 출력하기 12/print-star-12.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n = 0;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i == 0 || i == j || j % 2 == 1) {
+                printf("* ");
+            }
+            else {
+                printf("  ");
+            }
+        }
+    }
+    return 0;
+}
