6a1423fa9e7c39d8855d6f5e21dca4e5f4a71c94
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-08-01 12:49:54 +0900

    Title: 좌우 지그재그 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240801/좌우 지그재그 출력/print-lr-zigzag.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n = 0;
+    scanf("%d", &n);
+
+    int cnt = 0;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+            if (i % 2 == 0) {
+                ++cnt;
+            }
+            printf("%d ", cnt);
+            if (i % 2) {
+                --cnt;
+            }
+            
+        }
+        cnt += n;
+        printf("\n");
+    }
+
+    return 0;
+}
