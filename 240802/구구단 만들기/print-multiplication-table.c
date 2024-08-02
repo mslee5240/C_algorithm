f56c442737ee1bbf0f47cacbb221c9b8b5f4029c
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-08-02 15:07:00 +0900

    Title: 구구단 만들기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240802/구구단 만들기/print-multiplication-table.c
+++ 240802/구구단 만들기/print-multiplication-table.c
@@ -8,12 +8,12 @@ int main() {
     for (int i = 1; i <= 9; ++i) {
         for (int j = b; j >= a; j -= 2) {
             printf("%d * %d = %d", j, i, j * i);
-            if (j > 2) {
+            if (j > 2 || a != b) {
                 printf(" / ");
             }
         }
         printf("\n");
     }
-    
+
     return 0;
 }
