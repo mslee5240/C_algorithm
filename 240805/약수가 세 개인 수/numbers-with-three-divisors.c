be168cfb8731d79100605f1c01113e5374a8fe09
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-08-05 15:23:54 +0900

    Title: 약수가 세 개인 수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240805/약수가 세 개인 수/numbers-with-three-divisors.c
+++ 240805/약수가 세 개인 수/numbers-with-three-divisors.c
@@ -9,7 +9,7 @@ int main() {
 
     for (int i = a; i <= b; ++i) {
         int divisor_cnt = 0;
-        for (int j = 1; j <= a; ++j) {
+        for (int j = 1; j <= i; ++j) {
             if (i % j == 0) {
                 divisor_cnt++;
             }
