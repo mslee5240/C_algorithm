946060c91cb575dad198ad95eb599597345c4c58
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-08-06 15:38:30 +0900

    Title: 원소 10개의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240806/원소 10개의 합/sum-of-10-elements.c
+++ 240806/원소 10개의 합/sum-of-10-elements.c
@@ -2,15 +2,15 @@
 
 int main() {
     // 여기에 코드를 작성해주세요.
+    int arr[10] = { 0 };
     int sum_val = 0;
 
     for (int i = 0; i < 10; ++i) {
-        int n = 0;
-        scanf("%d%*c", &n);
-        sum_val += n;
+        scanf("%d%*c", &arr[i]);
+        sum_val += arr[i];
     }
 
     printf("%d\n", sum_val);
-    
+
     return 0;
 }
