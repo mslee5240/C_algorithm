be8ba4b313902c62c3199295b11bf184fe664721
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-08-06 16:30:34 +0900

    Title: 학점 계산기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240806/학점 계산기/credit-calculator.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n = 0;
+    scanf("%d", &n);
+
+    double dArr[5] = { 0.0 };
+    double sum_val = 0.0;
+
+    for (int i = 0; i < n; ++i) {
+        scanf("%lf%*c", &dArr[i]);
+        sum_val += dArr[i];
+    }
+
+    double avg = sum_val / n;
+
+    printf("%.1lf\n", avg);
+
+    if (avg > 4.0) {
+        printf("Perfect");
+    }
+    else if (avg > 3.0) {
+        printf("Good");
+    }
+    else {
+        printf("Poor");
+    }
+
+    return 0;
+}
