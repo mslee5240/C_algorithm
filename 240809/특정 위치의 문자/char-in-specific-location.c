42d095fff950cbfd0ab1a568c30c2e7076fb3537
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-08-09 00:45:00 +0900

    Title: 특정 위치의 문자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240809/특정 위치의 문자/char-in-specific-location.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char chArr[6] = { 'L', 'E', 'B', 'R', 'O', 'S' };
+
+    char ch = 0;
+    scanf("%c", &ch);
+
+    for (int i = 0; i < 6; ++i) {
+        if (chArr[i] == 'L') {
+            printf("%d", i);
+            break;
+        }
+        else {
+            printf("None");
+        }
+    }
+
+    return 0;
+}
