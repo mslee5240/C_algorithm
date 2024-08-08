b1f24c6e23bef8af252b82bcf51a3425b080aeab
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-08-09 00:19:30 +0900

    Title: 나눗셈의 나머지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240809/나눗셈의 나머지/remainder-of-division.c
+++ 240809/나눗셈의 나머지/remainder-of-division.c
@@ -8,9 +8,8 @@ int main() {
     scanf("%d %d", &nArr[0], &nArr[1]);
 
     while (nArr[0] > 1) {
-        int tmp = nArr[0];
+        ++cntArr[nArr[0] % nArr[1]];
         nArr[0] /= nArr[1];
-        ++cntArr[tmp % nArr[1]];
     }
 
     int sum_val = 0;
