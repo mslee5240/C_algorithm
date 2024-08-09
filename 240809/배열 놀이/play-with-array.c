488a7447b3f140a162d60fde24b62a9c65d29e45
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-08-09 14:03:35 +0900

    Title: 배열 놀이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240809/배열 놀이/play-with-array.c
+++ 240809/배열 놀이/play-with-array.c
@@ -33,7 +33,7 @@ int main() {
         }
         else {
             for (int j = qArr[1] - 1; j < qArr[2]; ++j) {
-                printf("%d ", nArr[j]);
+                printf("%d\n", nArr[j]);
             }
         }
     }
