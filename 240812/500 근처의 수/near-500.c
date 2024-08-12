37e980d660f63cac816414c19c48ee5ccd821f4f
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-08-12 20:26:24 +0900

    Title: 500 근처의 수, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240812/500 근처의 수/near-500.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int nArr[10] = { 0 };
+    int max_500 = 0, min_500 = 0;
+    for (int i = 0; i < 10; ++i) {
+        scanf("%d", &nArr[i]);
+        if (nArr[i] < 500 && nArr[i] > min_500) {
+            min_50 = nArr[i];
+        }
+    
+    }
+    return 0;
+}
