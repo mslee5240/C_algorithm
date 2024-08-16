3ee7d61462c3dae8aad5a22d1f7c8d81c1469cec
Minsoo <80834395+mslee5240@users.noreply.github.com>
2024-08-17 00:05:38 +0900

    Title: 대문자로 바꾸기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240817/대문자로 바꾸기/change-to-capital.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    char chArr[5][3];
+
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            scanf("%c%*c", &chArr[i][j]);
+        }
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            printf("%c ", chArr[i][j] + 32);
+        }
+        printf("\n");
+    }
+    return 0;
+}
