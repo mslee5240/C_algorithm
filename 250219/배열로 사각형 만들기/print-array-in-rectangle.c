dfe3f17eb824d2e473bd6b5e111a9400ad8329bf
Minsoo <80834395+mslee5240@users.noreply.github.com>
2025-02-19 13:59:16 +0900

    Title: 배열로 사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250219/배열로 사각형 만들기/print-array-in-rectangle.c
+++ 250219/배열로 사각형 만들기/print-array-in-rectangle.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int n = 3;
-    int arr_2d[3][3] = { 0 };
-    int a = 1;
-
+    int n = 5;
+    int arr_2d[5][5] = { 0 };
+    
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            arr_2d[i][j] = a;
+        arr_2d[0][i] = 1;
+        arr_2d[i][0] = 1;
+    }
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < n; j++) {
+            arr_2d[i][j] = arr_2d[i - 1][j] + arr_2d[i][j - 1];
         }
-        a += 2;
     }
 
     for (int i = 0; i < n; i++) {
