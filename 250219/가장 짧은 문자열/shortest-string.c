b410e7bfe1da7e253694a8e78ca12895f818a265
Minsoo <80834395+mslee5240@users.noreply.github.com>
2025-02-19 19:05:12 +0900

    Title: 가장 짧은 문자열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 250219/가장 짧은 문자열/shortest-string.c
+++ 250219/가장 짧은 문자열/shortest-string.c
@@ -2,30 +2,25 @@
 #include <string.h>
 
 int main() {
-    // Please write your code here.
     char chArr_1[20] = { 0 };
     char chArr_2[20] = { 0 };
     char chArr_3[20] = { 0 };
 
-    scanf("%s", chArr_1);
-    scanf("%s", chArr_2);
-    scanf("%s", chArr_3);
+    // 안전한 입력을 위해 %19s 사용
+    scanf("%19s", chArr_1);
+    scanf("%19s", chArr_2);
+    scanf("%19s", chArr_3);
 
-    int nArr[3] = { 0 };
-    nArr[0] = strlen(chArr_1), nArr[1] = strlen(chArr_2), nArr[2] = strlen(chArr_3);
+    int nArr[3] = { strlen(chArr_1), strlen(chArr_2), strlen(chArr_3) };
 
-    int max = 0;
-    int min = 20;
-    for (int i = 0; i < 3; i++) {
-        if (nArr[i] > max) {
-            max = nArr[i];
-        }
-        if (nArr[i] < min) {
-            min = nArr[i];
-        }
+    // 배열의 첫 번째 요소를 초기값으로 설정
+    int max = nArr[0], min = nArr[0];
+
+    for (int i = 1; i < 3; i++) {
+        if (nArr[i] > max) max = nArr[i];
+        if (nArr[i] < min) min = nArr[i];
     }
 
     printf("%d", max - min);
-
     return 0;
 }
